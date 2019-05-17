3b263327494a54fb470d65179989d903fa8573ac
lzkelley <lkelley@cfa.harvard.edu>
2019-05-17 14:16:50 -0500

    Add boilerplate

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('requirements.txt') as inn:
+    requirements = inn.read().splitlines()
+
+with open("README.md", "r") as inn:
+    long_description = inn.read().strip()
+
+with open('kdes/VERSION.txt') as inn:
+    version = inn.read().strip()
+
+setup(
+    name="kdes",
+    version=version,
+    author="Luke Zoltan Kelley",
+    author_email="lzkelley@northwestern.edu",
+    description=("Kernel density estimation and sampling."),
+    license="MIT",
+    keywords="",
+    url="https://github.com/lzkelley/kdes/",
+    packages=['kdes'],
+    include_package_data=True,
+    install_requires=requirements,
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
