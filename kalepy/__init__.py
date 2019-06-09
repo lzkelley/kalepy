21d8621bfdebeacf064656737812501cb4da67d5
lzkelley <lkelley@cfa.harvard.edu>
2019-06-09 12:23:29 -0500

    DOCS: describe __init__ parameter

--- kalepy/__init__.py
+++ kalepy/__init__.py
@@ -13,6 +13,7 @@ __copyright__ = "Copyright 2019 - Luke Zoltan Kelley and contributors"
 __contributors__ = []
 __bibtex__ = """"""
 
+# Numerical padding parameter (e.g. to avoid edge issues, etc)
 _NUM_PAD = 1e-8
 # Zero-out the PDF of kernels with infinite-support beyond this probability
 _TRUNCATE_INFINITE_KERNELS = 1e-6
