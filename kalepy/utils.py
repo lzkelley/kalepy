c4b1319417018cf7431e2e4a2d65b4e4ddf38a0f
lzkelley <lkelley@cfa.harvard.edu>
2019-06-17 13:18:15 -0700

    BUG: invalid kwarg was leading to error

--- kalepy/utils.py
+++ kalepy/utils.py
@@ -592,7 +592,7 @@ def _guess_str_format_from_range(arr, prec=2, log_limit=2, allow_int=True):
     """
 
     try:
-        extr = np.log10(np.fabs(minmax(arr, filter='ne')))
+        extr = np.log10(np.fabs(minmax(arr[arr != 0.0])))
     # string values will raise a `TypeError` exception
     except (TypeError, AttributeError):
         return ":"
