e7ca00818e2699946166d578be144aed9cd1c71c
lzkelley <lzkelley@gmail.com>
2020-03-25 13:25:14 -0500

    updates

--- venv_test.sh
+++ venv_test.sh
@@ -1,4 +1,5 @@
-python3.7 -m venv venv/
+python3.5 -m venv venv/
 source venv/bin/activate
 pip install -r requirements.txt
 bash tester.sh
+# nosetests --with-coverage --cover-inclusive --with-doctest --cover-package=kalepy /Users/lzkelley/Programs/kalepy/kalepy/tests/test_notebooks/test_utils.py
