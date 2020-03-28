3f0e6550e8532799aa3a321cd1f3f449bd2894a2
lzkelley <lzkelley@gmail.com>
2020-03-28 15:58:57 -0500

    Add timing plugin for nosetests

--- tester.sh
+++ tester.sh
@@ -1,2 +1,8 @@
 python build_notebook_tests.py
-nosetests --with-coverage --cover-inclusive --with-doctest --cover-package=kalepy kalepy/
+
+
+# To fail tests that take longer than (e.g.) 1.0 seconds:
+#   `--timer-warning 5.0 --timer-fail error`
+# To avoid logging messages (particularly from `matplotlib` use:
+#   `--nologcapture`
+nosetests --with-coverage --cover-inclusive --with-doctest --cover-package=kalepy --nologcapture --with-timer --timer-top-n 10 kalepy/
--- venv_test.sh
+++ venv_test.sh
@@ -2,4 +2,3 @@ python3.5 -m venv venv/
 source venv/bin/activate
 pip install -r requirements.txt
 bash tester.sh
-# nosetests --with-coverage --cover-inclusive --with-doctest --cover-package=kalepy /Users/lzkelley/Programs/kalepy/kalepy/tests/test_notebooks/test_utils.py
