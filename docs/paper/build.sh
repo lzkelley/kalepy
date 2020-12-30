1d1b84f6142f98280b090b40f5515922cca19912
lzkelley <lzkelley@gmail.com>
2020-12-30 11:22:22 -0600

    Fix deprecation from newer pandoc

--- docs/paper/build.sh
+++ docs/paper/build.sh
@@ -11,4 +11,4 @@ ENGINE="xelatex"
 OPTS="-V geometry:margin=1in"
 
 # pandoc --filter pandoc-citeproc --bibliography=paper.bib --pdf-engine=xelatex -s paper.md -o paper.pdf
-pandoc --filter pandoc-citeproc ${OPTS} --bibliography=${INPUT_BIB} --pdf-engine=${ENGINE} -s ${INPUT_TXT} -o ${OUTPUT_PDF}
+pandoc --citeproc ${OPTS} --bibliography=${INPUT_BIB} --pdf-engine=${ENGINE} -s ${INPUT_TXT} -o ${OUTPUT_PDF}
