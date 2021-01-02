502b6f17ad55d1c8d07182f5c1ed76ef3a02c563
lzkelley <lzkelley@gmail.com>
2021-01-02 15:54:09 -0600

    BUG: trying to fix 'No module named 'kalepy'' error on readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../kalepy/'))
+# sys.path.insert(0, os.path.abspath('../../kalepy/'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
