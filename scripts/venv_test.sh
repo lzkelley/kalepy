9bf45bd61af0bf751ac83c9f43c9fb2f6e99aaba
Luke Zoltan Kelley <lzkelley@gmail.com>
2022-06-07 13:30:23 -0500

    MAINT

--- scripts/venv_test.sh
+++
@@ -1,8 +0,0 @@
-rm -r venv/
-# python3.5 -m venv venv/
-python3.7 -m venv venv/
-source venv/bin/activate
-pip install -r requirements.txt
-# bash tester.sh
-python convert_notebook_tests.py
-pytest
