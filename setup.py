96932a279f0a8e3697d9e8bf110b2180c10affcb
Luke Zoltan Kelley <lzkelley@gmail.com>
2022-07-15 14:22:11 -0500

    Add publish-to-pypi action to automatically distribute on tagged main versions

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-"""
+"""Kernel Density Estimation (KDE) and sampling.
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+short_description = __doc__.strip()
 
 with open('requirements.txt') as inn:
     requirements = inn.read().splitlines()
@@ -14,18 +16,21 @@ with open('kalepy/VERSION.txt') as inn:
 
 setup(
     name="kalepy",
-    version=version,
     author="Luke Zoltan Kelley",
     author_email="lzkelley@northwestern.edu",
-    description=("Kernel density estimation and sampling."),
-    license="MIT",
     url="https://github.com/lzkelley/kalepy/",
+    version=version,
+    description=short_description,
     download_url="https://github.com/lzkelley/kalepy/archive/v{}.tar.gz".format(version),
-    packages=['kalepy'],
+    license="MIT",
+    packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
     long_description=long_description,
     long_description_content_type='text/markdown',
+    # Python version restrictions
+    python_requires=">=3.7",
+
     keywords=['utilities', 'physics', 'astronomy', 'cosmology',
               'astrophysics', 'statistics',
               'kernel density estimation', 'kernel density estimate'],
